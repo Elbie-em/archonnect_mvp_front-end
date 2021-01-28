2b1152faad6205ca8833a51cc33955345b039918
Elbie-em <emoonga1993@gmail.com>
2021-01-28 20:25:50 +0200

    Add routing and redux boiler-plate

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import store from './redux/store'
+import { Provider } from 'react-redux';
+import { BrowserRouter, Redirect, Route, Switch, NavLink } from 'react-router-dom';
+
 
 const App = () => {
+
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <Provider store={store}>
+        <div className="App">
+        <Switch>
+          
+        </Switch>
+        </div>
+      </Provider>
+    </BrowserRouter>
   );
 }
 
