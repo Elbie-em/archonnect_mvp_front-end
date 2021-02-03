8dc4bd0392fc9b97907228201081285df1ec23a0
Elbie-em <emoonga1993@gmail.com>
2021-02-03 23:22:50 +0200

    Create module for holding static review data

---
+++ src/staticData/reviewInfo.js
@@ -0,0 +1,34 @@
+const reviewInfo = [
+  {
+    img_url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBkqCNfw7uMqKtPZwi0Thv8GzsZiptsbV4fQ&usqp=CAU",
+    title:"Engineer",
+    name: "Andy Svestov",
+    comment:"Lorem Ipsum is simply a good way"
+  },
+  {
+    img_url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBkqCNfw7uMqKtPZwi0Thv8GzsZiptsbV4fQ&usqp=CAU",
+    title:"Engineer",
+    name: "Andy Svestov",
+    comment:"Lorem Ipsum is simply a good way"
+  },
+  {
+    img_url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBkqCNfw7uMqKtPZwi0Thv8GzsZiptsbV4fQ&usqp=CAU",
+    title:"Engineer",
+    name: "Andy Svestov",
+    comment:"Lorem Ipsum is simply a good way"
+  },
+  {
+    img_url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBkqCNfw7uMqKtPZwi0Thv8GzsZiptsbV4fQ&usqp=CAU",
+    title:"Engineer",
+    name: "Andy Svestov",
+    comment:"Lorem Ipsum is simply a good way"
+  },
+  {
+    img_url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBkqCNfw7uMqKtPZwi0Thv8GzsZiptsbV4fQ&usqp=CAU",
+    title:"Engineer",
+    name: "Andy Svestov",
+    comment:"Lorem Ipsum is simply a good way"
+  },
+]
+
+export default reviewInfo
