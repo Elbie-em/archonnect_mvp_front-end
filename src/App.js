428031fad7e2db3d1f4d08624bc701f550116bf6
Elbie-em <emoonga1993@gmail.com>
2021-02-04 18:58:30 +0200

    Add more routes for sign up and sign in pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import store from './redux/store'
 import { Provider } from 'react-redux';
 import { BrowserRouter, Redirect, Route, Switch, NavLink } from 'react-router-dom';
 import Home from './components/Home';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
 
 
 const App = () => {
@@ -13,6 +15,8 @@ const App = () => {
         <div className="App">
         <Switch>
           <Route exact path={"/"} component={Home}/>
+          <Route exact path={"/signup"} component={SignUp}/>
+          <Route exact path={"/signin"} component={SignIn}/>
         </Switch>
         </div>
       </Provider>
