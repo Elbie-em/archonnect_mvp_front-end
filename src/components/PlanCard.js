78a7bc9b0e97c06738c0b52fdb785035e6815079
Elbie-em <emoonga1993@gmail.com>
2021-02-05 00:06:11 +0200

    Build plan card

---
+++ src/components/PlanCard.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+const PlanCard = ({ img_url, name, price }) => {
+  return (
+    <div className="shadow-lg rounded plan-card">
+      <img className="plan-design" src={img_url} alt={img_url} />
+      <div className="d-flex flex-column p-3">
+        <div className="plan-info">
+          <h6 className="custom-font-a">{name}</h6>
+          <h6 className="al-right custom-font-a">${price}</h6>
+        </div>
+        <div className="plan-info">
+          <Link className="btn btn-warning rounded-pill xs-font text-white" to={"/houseplans"}>View</Link>
+          <h6 className="al-right custom-font-b xs-font">current bid</h6>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default PlanCard
