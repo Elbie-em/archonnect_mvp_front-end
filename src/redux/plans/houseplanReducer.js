7a3bb2adaf0df1d0feda4933ecfdf68ce0c4dad9
Elbie-em <emoonga1993@gmail.com>
2021-02-05 00:03:50 +0200

    Build plan reducer

---
+++ src/redux/plans/houseplanReducer.js
@@ -0,0 +1,35 @@
+import { GET_PLANS_FAILURE,GET_PLANS_SUCCESS, GET_PLANS_REQUEST } from "./housplanTypes"
+
+const initialState = {
+  loading: true,
+  data: [],
+  errorMsg: '',
+}
+
+const houseplanReducer = (state = initialState, action) => {
+  switch(action.type){
+    case GET_PLANS_REQUEST:
+      return {
+        ...state,
+        loading: true
+      }
+    case GET_PLANS_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        data: action.payload,
+        errorMsg: ''
+      }
+    case GET_PLANS_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        data: [],
+        errorMsg: action.payload
+      }
+    default:
+      return state
+  }
+}
+
+export default houseplanReducer
