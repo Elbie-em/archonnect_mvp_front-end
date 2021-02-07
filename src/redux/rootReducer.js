5a95e02afbc0705d09c6e01305f81771204abb81
Elbie-em <emoonga1993@gmail.com>
2021-02-07 16:22:12 +0200

    Add loggedIn reducer to root reducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
+import loggedInReducer from './sessions/loggedInReducer'
 import houseplanReducer from './plans/houseplanReducer'
 
 const rootReducer = combineReducers({
+  loggedIn: loggedInReducer,
   plans:houseplanReducer,
 });
 
