2dcbf2edb30a4849afc151eb92c349515a793b50
Elbie-em <emoonga1993@gmail.com>
2021-02-07 16:20:05 +0200

    Create registration types and registration actions to register users

---
+++ src/redux/registrations/registrationActions.js
@@ -0,0 +1,38 @@
+import { CREATE_USER_FAILURE, CREATE_USER_REQUEST, CREATE_USER_SUCCESS } from "./registrationTypes"
+import axios from 'axios'
+
+export const createUserRequest = () => {
+  return {
+    type:CREATE_USER_REQUEST
+  }
+}
+
+export const createUserSuccess = (response) => {
+  return {
+    type:CREATE_USER_SUCCESS,
+    payload: response
+  }
+}
+export const createUserFailure = (error) => {
+  return {
+    type:CREATE_USER_FAILURE,
+    payload: error
+  }
+}
+
+export const createUser = (data) => {
+  return (dispatch) => {
+    dispatch(createUserRequest())
+    axios.post("http://localhost:3001/api/v1/registrations",
+    data,
+    {
+      withCredentials: true
+    })
+    .then(response => {
+       dispatch(createUserSuccess(response.data))
+    }).catch(error => {
+      console.log(error)
+      dispatch(createUserFailure(error))
+    })
+  }
+}
