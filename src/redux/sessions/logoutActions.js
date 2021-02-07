9fde746bf38d666b303841e6d16267175a155e67
Elbie-em <emoonga1993@gmail.com>
2021-02-07 16:26:36 +0200

    Create logout actions and logout types

---
+++ src/redux/sessions/logoutActions.js
@@ -0,0 +1,34 @@
+import { LOG_OUT_FAILURE, LOG_OUT_REQUEST, LOG_OUT_SUCCESS } from "./logoutTypes"
+
+import axios from 'axios'
+
+export const logoutRequest = () => {
+  return {
+    type: LOG_OUT_REQUEST
+  }
+}
+
+export const logoutSuccess = (response) => {
+  return {
+    type: LOG_OUT_SUCCESS,
+    payload: response
+  }
+}
+export const logoutFailure = (error) => {
+  return {
+    type: LOG_OUT_FAILURE,
+    payload: error
+  }
+}
+
+export const logout = (data) => {
+  return (dispatch) => {
+    dispatch(logoutRequest())
+    axios.delete(`http://localhost:3001/api/v1/logout`)
+      .then(response => {
+        dispatch(logoutSuccess(response.data))
+      }).catch(error => {
+        dispatch(logoutFailure(error))
+      })
+  }
+}
