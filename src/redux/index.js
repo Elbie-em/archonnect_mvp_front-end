637ee7d9f275a13a6cb68fd6f1dc287e0fc42cc5
Elbie-em <emoonga1993@gmail.com>
2021-02-07 16:24:14 +0200

    Additional export of Actions

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,1 +1,6 @@
 export * from './plans/houseplanActions'
+export * from './registrations/registrationActions'
+export * from './user/userActions'
+export * from './sessions/loggedInActions'
+export * from './sessions/logInActions'
+export * from './sessions/logoutActions'
