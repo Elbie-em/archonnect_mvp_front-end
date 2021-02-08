0e09c5b7c0f81dc6ee6896868b22504296e8f2c9
Elbie-em <emoonga1993@gmail.com>
2021-02-08 21:09:59 +0200

    Add plan reducer to rootReducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import loggedInReducer from './sessions/loggedInReducer'
 import houseplanReducer from './plans/houseplanReducer'
+import planReducer from './plan/planReducer';
 
 const rootReducer = combineReducers({
   loggedIn: loggedInReducer,
   plans:houseplanReducer,
+  plan: planReducer
 });
 
 export default rootReducer;
