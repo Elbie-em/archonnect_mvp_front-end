79c44380bdeddeb030c96f4b37c2d46756168b40
Elbie-em <emoonga1993@gmail.com>
2021-02-09 00:34:55 +0200

    Add id prop to plan card

--- src/components/PlanCard.js
+++ src/components/PlanCard.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
-const PlanCard = ({ img_url, name, price }) => {
+const PlanCard = ({id,img_url, name, price }) => {
   return (
     <div className="shadow-lg rounded plan-card">
       <img className="plan-design" src={img_url} alt={img_url} />
@@ -11,7 +11,7 @@ const PlanCard = ({ img_url, name, price }) => {
           <h6 className="al-right custom-font-a">${price}</h6>
         </div>
         <div className="plan-info">
-          <Link className="btn btn-start rounded-pill xs-font text-white" to={"/houseplans"}>View</Link>
+          <Link className="btn btn-start rounded-pill xs-font text-white" to={`/houseplans/${id}`}>View</Link>
           <h6 className="al-right custom-font-b xs-font">current bid</h6>
         </div>
       </div>
