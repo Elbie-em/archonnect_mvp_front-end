0d632f513d1ef3e7509bda287969fc8dbc712ef5
Elbie-em <emoonga1993@gmail.com>
2021-02-10 02:16:47 +0200

    Add favourite reducer to rootReducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import loggedInReducer from './sessions/loggedInReducer'
 import houseplanReducer from './plans/houseplanReducer'
 import planReducer from './plan/planReducer';
+import favouriteRedcuer from './favourites/favouriteReducer'
 
 const rootReducer = combineReducers({
   loggedIn: loggedInReducer,
   plans:houseplanReducer,
-  plan: planReducer
+  plan: planReducer,
+  favourites: favouriteRedcuer,
 });
 
 export default rootReducer;
