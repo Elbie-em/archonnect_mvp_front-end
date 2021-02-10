84fc51da0a3637b2b34b0fd119040c4f03279f10
Elbie-em <emoonga1993@gmail.com>
2021-02-10 02:21:00 +0200

    Update class names

--- src/components/ReviewCard.js
+++ src/components/ReviewCard.js
@@ -3,7 +3,7 @@ import React from 'react'
 const ReviewCard = ({img_url,title,name,comment}) => {
   return (
     <>
-      <div className="shadow-lg review-card rounded">
+      <div className="shadow review-card rounded">
         <img className="rc-img rounded-circle" src={img_url} alt="img_url"/>
         <h5 className="custom-font-a">{name}</h5>
         <p className="text-muted custom-font-b xs-font">{title}</p>
