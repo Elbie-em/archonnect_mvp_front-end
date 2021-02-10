23a502b360274f37e1b4d06cfb25025eea33a42b
Elbie-em <emoonga1993@gmail.com>
2021-02-10 02:22:39 +0200

    Build handle create favourite function

--- src/containers/Plan.js
+++ src/containers/Plan.js
@@ -3,21 +3,45 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom'
 import LoadingSpinner from '../components/LoadingSpinner';
 import SPlanCard from '../components/SPlanCard';
-import {getPlan} from '../redux'
+import {createFavourite, getPlan, checkLoggedInStatus} from '../redux'
 
-const Plan = ({ match, plan, getPlan}) => {
+const Plan = ({ match, plan, getPlan,createFavourite,loggedInStatus, isLoggedIn}) => {
   const planId = match.params.id;
+  let user_id = "";
   useEffect(() => {
     getPlan(planId)
-  }, [getPlan]);
+    isLoggedIn()
+  }, [getPlan,isLoggedIn]);
 
-  
+  if (loggedInStatus.logged_in) {
+    user_id = loggedInStatus.user.id
+  }
+
+  const handleCreateFavourite = () => {
+    const favData = { data: { user_id: user_id, plan_id: planId } }
+    createFavourite(favData)
+    .then(response =>{
+      const res = response.payload
+      let message = ""
+      if(res.status == 202 ){
+        message = res.message
+        alert(message)
+      }else if(res.status == 500){
+        message = res.message
+        alert(message)
+      }
+    }).catch(err => {
+      console.log(err)
+    })
+  }
+
+
   const showPlan = () => {
     if (!_.isEmpty(plan.data.result) && plan.data.status === 202) {
       const item = plan.data.result
       return (
         <>
-           <SPlanCard name={item.name} details={item.details} architect={item.architect_name} rating={item.rating} price={item.price} imgF={item.design_img_url} imgS={item.blueprint_one_url} imgT={item.blueprint_two_url} />
+           <SPlanCard name={item.name} details={item.details} architect={item.architect_name} rating={item.rating} price={item.price} imgF={item.design_img_url} imgS={item.blueprint_one_url} imgT={item.blueprint_two_url} createFavourite={handleCreateFavourite}/>
         </>
       )
     }
@@ -50,10 +74,13 @@ const Plan = ({ match, plan, getPlan}) => {
 
 const mapStateToProps = state => ({
   plan: state.plan,
+  loggedInStatus: state.loggedIn.data,
 });
 
 const mapDispatchToProps = dispatch => ({
   getPlan: id => dispatch(getPlan(id)),
+  createFavourite: data => dispatch(createFavourite(data)),
+  isLoggedIn: () => dispatch(checkLoggedInStatus()),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Plan)
